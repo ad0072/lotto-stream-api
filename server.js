59f53679a142f16e255c97f61cc868ea7eb1e4b4
ad0072 <70003842+ad0072@users.noreply.github.com>
2026-05-14 12:09:07 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,67 +1,13 @@
 import express from "express";
-import { chromium } from "playwright";
 
 const app = express();
 
 app.get("/", async (req, res) => {
 
-  let browser;
-
-  try {
-
-    browser = await chromium.launch({
-      headless: true,
-      args: [
-        "--no-sandbox",
-        "--disable-setuid-sandbox"
-      ]
-    });
-
-    const page = await browser.newPage();
-
-    let urls = [];
-
-    page.on("response", async (response) => {
-
-      const url = response.url();
-
-      if (
-        url.includes(".m3u8") ||
-        url.includes("/api/stream/") ||
-        url.includes("playlist")
-      ) {
-        urls.push(url);
-      }
-
-    });
-
-    await page.goto("https://exphuay.com", {
-      waitUntil: "domcontentloaded",
-      timeout: 120000
-    });
-
-    await page.waitForTimeout(10000);
-
-    await browser.close();
-
-    return res.json({
-      status: "success",
-      total: urls.length,
-      urls
-    });
-
-  } catch (err) {
-
-    if (browser) {
-      await browser.close();
-    }
-
-    return res.json({
-      status: "error",
-      message: err.message
-    });
-
-  }
+  return res.json({
+    status: "success",
+    message: "server working"
+  });
 
 });
 
